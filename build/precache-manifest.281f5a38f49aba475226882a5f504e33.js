842cbf90b005f3253250cacfbcfb829ec4c146e6
enkztw <jztenk@gmail.com>
2019-11-13 16:04:32 +0300

    Deployment testement

---
+++ build/precache-manifest.281f5a38f49aba475226882a5f504e33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22c231f864fa4b65df07a14fa79db75e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "388b1ad3a4b7afeaff49",
+    "url": "/static/css/main.ebcda4ca.chunk.css"
+  },
+  {
+    "revision": "22be2eb5dae8d86de288",
+    "url": "/static/js/2.73d32c96.chunk.js"
+  },
+  {
+    "revision": "388b1ad3a4b7afeaff49",
+    "url": "/static/js/main.434976bd.chunk.js"
+  },
+  {
+    "revision": "442a6bb76e0242c4a206",
+    "url": "/static/js/runtime-main.5ee29ae7.js"
+  }
+]);
