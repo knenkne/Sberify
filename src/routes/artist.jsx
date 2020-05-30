6118e3c3590cc85313f7aa5c87a14479d6a893b1
KuznetsovMA <jztenk@gmail.com>
2020-05-30 18:34:11 +0300

    Prerenderer & Helmet

--- src/routes/artist.jsx
+++ src/routes/artist.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import { Helmet } from 'react-helmet'
 import { connect } from 'react-redux'
 import { actions } from '../store'
 import { Redirect } from 'react-router-dom'
@@ -48,63 +49,75 @@ export class Artist extends React.Component {
     }
 
     return (
-      <div className="container">
-        <section className="artist">
-          <div
-            className="artist__header"
-            style={{
-              backgroundImage: `url(${this.props.headerImage}`
-            }}
-          ></div>
-          <div className="artist__info">
-            <div className="artist__block">
-              <img
-                src={this.props.image}
-                alt={this.props.name}
-                className="artist__image"
-              />
-              <h2 className="artist__name">{this.props.name}</h2>
-              <ul className="artist__socials">
-                {Object.entries(this.props.socials).map(([name, userName]) => {
-                  if (!userName) {
-                    return ''
-                  }
-
-                  return (
-                    <Social
-                      key={name}
-                      data={socialsMap[name].data}
-                      isLooped={socialsMap[name].isLooped}
-                      link={`https://${name}.com/${userName}`}
-                    />
-                  )
-                })}
-              </ul>
-              <div className="artist__description-wrapper">
-                <div className="artist__description">
-                  {this.props.description.split('\n').map((item, index) => {
-                    return <p key={index}>{item}</p>
-                  })}
-                </div>
-              </div>
-              {this.props.video && <Video link={this.props.video} />}
-            </div>
-            <div className="artist__block">
-              {this.props.albums.length > 0 && (
-                <Songs
-                  title="Popular songs"
-                  songs={this.props.albums.slice(0, 3).map(album => ({
-                    ...album.songs[0],
-                    image: album.image
-                  }))}
-                  artist={this.props.name}
+      <React.Fragment>
+        <Helmet>
+          <title>{this.props.name}</title>
+          <meta property="og:url" content={window.location.href} />
+          <meta property="og:type" content="profile" />
+          <meta property="og:title" content={this.props.name} />
+          <meta property="og:description" content={this.props.description} />
+          <meta property="og:image" content={this.props.image} />
+        </Helmet>
+        <div className="container">
+          <section className="artist">
+            <div
+              className="artist__header"
+              style={{
+                backgroundImage: `url(${this.props.headerImage}`
+              }}
+            ></div>
+            <div className="artist__info">
+              <div className="artist__block">
+                <img
+                  src={this.props.image}
+                  alt={this.props.name}
+                  className="artist__image"
                 />
-              )}
-              <Albums albums={this.props.albums} />
+                <h2 className="artist__name">{this.props.name}</h2>
+                <ul className="artist__socials">
+                  {Object.entries(this.props.socials).map(
+                    ([name, userName]) => {
+                      if (!userName) {
+                        return ''
+                      }
+
+                      return (
+                        <Social
+                          key={name}
+                          data={socialsMap[name].data}
+                          isLooped={socialsMap[name].isLooped}
+                          link={`https://${name}.com/${userName}`}
+                        />
+                      )
+                    }
+                  )}
+                </ul>
+                <div className="artist__description-wrapper">
+                  <div className="artist__description">
+                    {this.props.description.split('\n').map((item, index) => {
+                      return <p key={index}>{item}</p>
+                    })}
+                  </div>
+                </div>
+                {this.props.video && <Video link={this.props.video} />}
+              </div>
+              <div className="artist__block">
+                {this.props.albums.length > 0 && (
+                  <Songs
+                    title="Popular songs"
+                    songs={this.props.albums.slice(0, 3).map(album => ({
+                      ...album.songs[0],
+                      image: album.image
+                    }))}
+                    artist={this.props.name}
+                  />
+                )}
+                <Albums albums={this.props.albums} />
+              </div>
             </div>
-          </div>
-        </section>
-      </div>
+          </section>
+        </div>
+      </React.Fragment>
     )
   }
 }
