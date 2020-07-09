8444dd5a9290610b4ed181a1b63e5b6e454c2517
KuznetsovMA <jztenk@gmail.com>
2020-07-09 18:20:43 +0300

    og

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
-
+import { Helmet } from 'react-helmet'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Provider } from 'react-redux'
+
 import { createStore } from './store'
 
 import Preloader from './components/preloader'
@@ -15,18 +16,31 @@ import './App.scss'
 
 function App() {
   return (
-    <Provider store={createStore()}>
-      <Router>
-        <Preloader />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/artist/:name" component={Artist} />
-          <Route path="/album/:name" component={Album} />
-          <Route path="/song/:name" component={Song} />
-          <Route component={NotFound} />
-        </Switch>
-      </Router>
-    </Provider>
+    <React.Fragment>
+      <Helmet>
+        <title>{this.props.name}</title>
+        <meta property="og:site_name" content="Sberify Music App" />
+        <meta property="og:type" content="music.radio_station" />
+        <meta property="og:title" content="Sberify" />
+        <meta property="og:description" content="Most awesome music app ever" />
+        <meta
+          property="og:image"
+          content="https://apptractor.ru/wp-content/uploads/2017/02/sber.png"
+        />
+      </Helmet>
+      <Provider store={createStore()}>
+        <Router>
+          <Preloader />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/artist/:name" component={Artist} />
+            <Route path="/album/:name" component={Album} />
+            <Route path="/song/:name" component={Song} />
+            <Route component={NotFound} />
+          </Switch>
+        </Router>
+      </Provider>
+    </React.Fragment>
   )
 }
 
